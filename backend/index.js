ab772a79f0456dd68cef95973f01524df76623bd
Natasha Moses <nem67127@gmail.com>
2022-07-30 12:54:01 -0700

    endpoints and handlers for GET items and GET item

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,44 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const { getItems, getItem } = require("./handlers");
 
 const PORT = 4000;
 
 express()
-  .use(function(req, res, next) {
+  .use(function (req, res, next) {
     res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+  .use("/", express.static(__dirname + "/"))
 
-  // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  //---------------REST endpoints ---------------------//
+  //GET all items
+  //returns an array if objects
+  .get("/api/items", getItems)
+
+  //GET a particular item based on ID
+  //returns an object
+  .get("/api/items/:itemId", getItem)
+
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
